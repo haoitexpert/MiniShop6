d5528c1b57a447110f41ff1aad093a82574f4d1a
HAO LUONG <luongvihao@outlook.com>
2026-02-07 12:02:21 +0700

    1_Create Project + Model + DbContext

--- Data/Migrations/20260207035002_addCategoryToDb.Designer.cs
+++ Data/Migrations/20260207035002_addCategoryToDb.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MiniShop6.Data;
 
@@ -11,9 +12,11 @@ using MiniShop6.Data;
 namespace MiniShop6.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260207035002_addCategoryToDb")]
+    partial class addCategoryToDb
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
